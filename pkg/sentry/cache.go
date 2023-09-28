b0790495a513e5df72d51670f0b54c49e95100e8
Oleg Yevik <olegyevik@canva.com>
2023-09-29 11:08:10 +1300

    Update pkg/sentry/cache.go
    Co-authored-by: Foad Nosrati Habibi <foadnh@canva.com>

--- pkg/sentry/cache.go
+++ pkg/sentry/cache.go
@@ -9,27 +9,27 @@ const (
 	defaultCacheTTL = 5 * time.Minute
 )
 
-type Cache struct {
+type Cache[T comparable] struct {
 	mu         sync.Mutex
-	data       map[string]interface{}
+	data       map[string]T
 	expiration map[string]time.Time
 }
 
-func NewCache() *Cache {
+func NewCache[T comparable]() *Cache {
 	return &Cache{
-		data:       make(map[string]interface{}),
+		data:       make(map[string]T),
 		expiration: make(map[string]time.Time),
 	}
 }
 
-func (c *Cache) Set(key string, value interface{}, ttl time.Duration) {
+func (c *Cache) Set[T comparable](key string, value T, ttl time.Duration) {
 	c.mu.Lock()
 	defer c.mu.Unlock()
 	c.data[key] = value
 	c.expiration[key] = time.Now().Add(ttl)
 }
 
-func (c *Cache) Get(key string) (interface{}, bool) {
+func (c *Cache) Get[T comparable](key string) (T, bool) {
 	c.mu.Lock()
 	defer c.mu.Unlock()
 	value, found := c.data[key]
